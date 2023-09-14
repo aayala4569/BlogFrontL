620bb89c091275ad4aecf2cd8377bdebe5aae834
aayala4569 <angelicaayala100@gmail.com>
2023-09-13 19:44:45 -0700

    created our modal

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -14,34 +14,52 @@ const Dashboard = () => {
     <>
       <Container>
       <Button className="me-3" variant="outline-primary" onClick={handleShow}>
-        Launch demo modal
+       Add Blog Item
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header style={{backgroundColor: "#2f2f2f"}} closeButton>
-          <Modal.Title style={{backgroundColor: "#2f2f2f"}}>Modal heading</Modal.Title>
+          <Modal.Title style={{backgroundColor: "#2f2f2f"}}>Add Blog Item</Modal.Title>
         </Modal.Header>
         <Modal.Body style={{backgroundColor: "#2f2f2f"}}>
         <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+      <Form.Group className="mb-3" controlId="Title">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="Text" placeholder="Enter Title" />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" controlId="Description">
+        <Form.Label>Description</Form.Label>
+        <Form.Control type="Text" placeholder="Enter Description" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+     
+      
     </Form>
+
+<Form.Group className="mb-3" controlId="Category">
+      <Form.Select aria-label="Default select example">
+      <option>Select Category</option>
+      <option value="Food">Food</option>
+      <option value="Fitness">Fitness</option>
+      <option value="Sport">Sports</option>
+      <option value="Tech">Tech</option>
+        </Form.Select>
+      </Form.Group>
+
+
+      <Form.Group className="mb-3" controlId="Tags">
+        <Form.Label>Tags</Form.Label>
+        <Form.Control type="text" placeholder="Enter Tags"/>
+        
+      </Form.Group>
+
+
+
+
+
         </Modal.Body>
         <Modal.Footer style={{backgroundColor: "#2f2f2f"}}>
           <Button variant="outline-secondary" onClick={handleClose}>
@@ -51,10 +69,13 @@ const Dashboard = () => {
             Save Changes
           </Button>
         </Modal.Footer>
+    
+
       </Modal>
         
+      
         
-            <Button variant="outline-primary">Add Blog Item</Button>
+            <Button variant="outline-primary">Edit Blog Item</Button>
         
       </Container>
     </>
